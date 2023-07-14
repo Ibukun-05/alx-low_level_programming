1d7be7373d4e64a8c24bda4b52679f2402d45feb
Ibukun-05 <ibukunadelaja3@gmail.com>
2023-07-14 12:35:59 -0700

    holy norms

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,7 +14,8 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-if ((n % 10) > 5)
+
+	if ((n % 10) > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n",
 			n, n % 10);
@@ -31,4 +32,5 @@ if ((n % 10) > 5)
 	}
 
 	return (0);
+
 }
